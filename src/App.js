494f9a2399f77dcf4d1fde390addecb6068da033
DaniAndhika <daniandhika9@gmail.com>
2022-07-07 20:38:07 +0700

    belajar_react

---
+++ src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useState } from "react"
+ 
+function App() {
+  const [products, setProducts] = useState([
+    {id: 1, title: 'Product 1', price: 899},
+    {id: 2, title: 'Product 2', price: 982},
+    {id: 3, title: 'Product 3', price: 322},
+    {id: 4, title: 'Product 4', price: 763},
+    {id: 5, title: 'Product 5', price: 389}
+  ]);
+ 
+  return (
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={ product.id }> { product.title } - { product.price } </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+ 
+export default App;
